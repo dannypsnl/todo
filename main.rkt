fdb020b81c73348ec746403c46bc049d9b81b586
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-22 00:02:32 +0800

    fix
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- main.rkt
+++ main.rkt
@@ -1,7 +1,14 @@
 #lang racket
 (provide todo)
-(require (for-syntax syntax/parse))
+(require (for-syntax syntax/parse
+                     racket/syntax-srcloc))
 
 (define-syntax (todo stx)
   (syntax-parse stx
-    [(_) (raise (format "~a" stx))]))
+    [(_) #`(raise (format "TODO ~a" #,((compose srcloc->string syntax-srcloc) stx)))]))
+
+(module+ test
+  (require rackunit)
+
+  (check-exn string? (lambda () (todo)))
+)
